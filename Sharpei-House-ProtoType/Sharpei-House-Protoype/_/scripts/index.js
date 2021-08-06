ce7bffb43e5d2716d567df8f2866bc5d189bcb12
maddernd <87599686+maddernd@users.noreply.github.com>
2021-08-06 17:19:00 +1000

    prototypeupload

---
+++ Sharpei-House-ProtoType/Sharpei-House-Protoype/_/scripts/index.js
@@ -0,0 +1,21 @@
+function menu() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+var button = document.createElement("button");
+button.innerHTML = "Who is the Sharpei Of the Week";
+
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+// 3. Add event handler
+button.addEventListener ("click", function() {
+  alert("This is a meat mouth sharpei called Bill");
+});
+
