a57bc376208b75c3608e4e7d26567f08f60f49ac
maddernd <87599686+maddernd@users.noreply.github.com>
2021-08-21 16:50:33 +1000

    Week5 tasks

---
+++ Week5/Task 2/task2-ComputedProperty-TakePhoto/src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+      <!--Search Box for Photos from Menu only displayed on desktop  -->
+      <input v-show="!mobile" type="text" placeholder="Search.." />
+      <take-photo></take-photo>
+  </div>
+</template>
+
+<script>
+import TakePhoto from './TakePhoto.vue';
+export default {
+  components: { TakePhoto },
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods: {
+     //return mobile when certain window size limit is reached
+    data() {
+      return {
+        mobile: window.innerWidth <= 759,
+      };
+    },
+    created() {
+      addEventListener("resize", () => {
+        this.mobile = innerWidth <= 759;
+      });
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
