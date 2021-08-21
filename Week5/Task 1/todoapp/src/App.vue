0178bcf873a9f83844c454b3666870315dc68b67
maddernd <87599686+maddernd@users.noreply.github.com>
2021-08-21 12:03:18 +1000

    weewk5

---
+++ Week5/Task 1/todoapp/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <todo-item
+      v-for="item in groceryList"
+      v-bind:todo="item"
+      v-bind:key="item.id"></todo-item>
+</div>
+</template>
+
+<script>
+
+import todoItem from './components/todo-item.vue'
+export default {
+  name: 'App',
+  components: {
+     todoitem 
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Week5/Task 1/todoapp/src/components/todo-item.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+<p>test</p>
+    <li> {{todo.text}}</li>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'todo-item',
+  props: [todo],
+  data: {
+      groceryList: [
+          {id: 0, text: 'Vegetables'},
+          {id: 1, text: 'Cheese'},
+          {id: 2, text: 'What ever humans are supposed to eat'}
+      ]
+  }
+}
+</script>
